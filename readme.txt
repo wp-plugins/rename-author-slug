dabb1e6fb7e0a27dd523937ae72523462413020d
mukto90 <mukto90@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-03 10:24:30 +0000

    New Plugin
    git-svn-id: https://plugins.svn.wordpress.org/rename-author-slug/trunk@959480 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== Rename Author Slug ===
+Contributors: mukto90
+Tags: permalink, author permalink, author url, author base, url base, permalink base, change author url, change permalink, author slug, author profile slug
+Requires at least: 3.0
+Tested up to: 3.9.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This simple plugin changes the author based permalink slug 'author' to anything you wish.
+
+== Description ==
+
+This simple plugin changes the user based permalink slug 'author' to anything you wish.
+For example, you have an e-learning management system where different members teach on different topics. Each member has his unique profile url (author permalink) something like this: http://mukto.medhabi.com/author/mukto90
+Now you wish the word 'author' to be changed to 'teacher'. So, this is the plugin you need.
+
+== Installation ==
+
+1. Upload the `rename-author-slug` folder and its contents to your `wp-contents/plugins` directory.
+2. Activate in the `Plugins` menu.
+3. Configure from WordPress admin area.
+4. You're done!
+
+== Screenshots ==
+
+1. Configure page
+2. Custom author slug
---
+++ rename-author-slug.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Plugin Name: Rename Author Slug
+Description: This simple plugin changes the author based permalink slug 'author' to anything you wish.
+Author: Nazmul Ahsan
+Plugin URI: https://wordpress.org/plugins/rename-author-slug/
+Author URI: http://mukto.medhabi.com
+Version: 1.0.0
+License: GPL2+
+Text Domain: MedhabiDotCom
+*/
+function author_base_url(){
+	add_menu_page( 'Author Slug', 'Author Slug', 'administrator', 'author_base', 'author_base_page', plugin_dir_url( __FILE__ ).'/images/icon.png', 10 );
+}
+add_action('admin_menu', 'author_base_url');
+function author_base_page(){
+	echo '<div class="wrap">';
+	echo "<h2>" . __( 'Author Slug', 'author-url' ) . "</h2>";
+?>
+<?php if($_POST){
+	$user_input = $_POST['author_url'];
+	update_option('author_permalink', $user_input)
+	?>
+<div class="updated below-h2" id="message">
+	<p>Author Slug Changed.</p>
+</div>
+	<?php
+}?>
+	<form action="" method="post">
+		<table class="form-table">
+			<tbody>
+				<tr valign="top">
+					<th scope="row"><label for="author_url">Type New Author Slug:</label></th>
+					<td><input type="text" name="author_url" id="author_url" value="<?php echo get_option('author_permalink'); ?>" placeholder="Example: blogger" size="20" /></td>
+				</tr>
+			</tbody>
+		</table>
+		<p class="submit">
+			<input type="submit" value="Save Changes" class="button button-primary" id="submit" name="submit">
+		</p>
+</form>
+</div>
+<?php
+}
+	function change_author_permalinks() {
+		global $wp_rewrite;
+		$wp_rewrite->author_base = get_option('author_permalink');
+		$wp_rewrite->flush_rules();
+	}
+	if(get_option('author_permalink') != ''){
+	add_action('init','change_author_permalinks');
+	}
+	else{
+		delete_option('author_permalink');
+		delete_option('rewrite_rules');
+	}
